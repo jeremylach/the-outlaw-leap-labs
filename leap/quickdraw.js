99b22b5bf0418e1d601846d16e61067ffc953c7b
Ryan DeBeasi <ryan@prod4ever.com>
2013-03-22 13:02:05 -0400

    Log direction of pointer finger

--- leap/quickdraw.js
+++ leap/quickdraw.js
@@ -2,6 +2,7 @@
 	window.onload = function() {
 		var log = document.getElementById('log'),
 			status = document.getElementById('status'),
+			directionElem = document.getElementById('direction'),
 			pointing = false,
 			controllerOptions = {enableGestures: false};
 
@@ -13,10 +14,28 @@
 			status.innerHTML = '<p>' + message + '</p>';		
 		}
 
+		function roundToOne(num) {
+			return Math.round(num * 100) / 100;
+		}
+
+		function wDirection(direction) {
+			directionElem.innerHTML = '';
+			if (direction === '') {
+				directionElem.innerHTML = "I can't see anything.";
+				return;
+			}
+			directionElem.innerHTML += " <b>X:</b> " + roundToOne(direction[0]);
+			directionElem.innerHTML += " <b>Y:</b> " + roundToOne(direction[1]);
+			directionElem.innerHTML += " <b>Z:</b> " + roundToOne(direction[2]);
+		}
+
 		wLog('Let\'s Ride!');
 
 		Leap.loop(controllerOptions, function(frame) {
-			var status = '';
+			var status = '',
+				gunFinger;
+
+			// Log data about the fingers we see.
 			if (frame.pointables.length === 0) {
 				status += 'I can\'t see you.';
 			} else {
@@ -26,19 +45,23 @@
 				status += ' You are pointing.';
 			}
 
-			wStatus(status);
 			if (frame.pointables.length === 1) {
+				gunFinger = frame.pointables[0];
+				wDirection(gunFinger.direction);
 				if (!pointing) {
 					wLog('BANG!');
+					console.dir(gunFinger.direction);
 				}
-				//pointing = true;
 			}
 
 			if (frame.pointables.length > 0) {
 				pointing = true;
 			} else {
 				pointing = false;
+				wDirection('');
 			}
+
+			wStatus(status);
 		});
 	}
 })();
